d2d5badd15444ac822da3c1ec6072401306752c5
ogirginc <ogirginc@gmail.com>
2016-07-19 17:34:42 +0100

    Working on Ch18

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,13 +1,8 @@
-# require "capybara"
-# require "selenium-webdriver"
-# include Capybara::DSL
-require 'spec_helper'
-
-feature 'Enter names', :type => :feature do
+feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
     fill_in :player_name, with: 'Dave'
-    # fill_in :player_2_name, with: 'Mittens'
+    fill_in :player_2_name, with: 'Mittens'
     click_button 'Submit'
     expect(page).to have_content 'Dave'
   end
