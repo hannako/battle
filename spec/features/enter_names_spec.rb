5c4825ce70f9b89ba657a5e67054a0135c87b3c7
Robert Summers <rsummers02@gmail.com>
2016-07-19 17:44:25 +0100

    ch 18 complete!

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,9 @@
 feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
+    fill_in :player_1_name, with: 'Ogi'
+    fill_in :player_2_name, with: 'Robert'
     click_button 'Submit'
-    expect(page).to have_content 'Dave'
+    expect(page).to have_content 'Ogi vs Robert'
   end
 end
