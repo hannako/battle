f620b503a19e5a58ebeaf6cbad5cea27d2ef9736
ogirginc <ogirginc@gmail.com>
2016-07-20 11:58:41 +0100

    Complete ch21: Test Helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Ogi'
-    fill_in :player_2_name, with: 'Robert'
-    click_button 'Submit'
-    expect(page).to have_content 'Ogi vs Robert'
+    sign_in_and_play
+    expect(page).to have_content 'Joe vs Oggie'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'See Player 2 hit points' do
   scenario 'show hit points of rival' do
-    visit('/')
-    fill_in :player_1_name, with: 'Joe'
-    fill_in :player_2_name, with: 'Oggie'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Oggie: 50HP"
   end
 end
