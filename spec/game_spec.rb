09b667d0028b671bf97faf89651c06b852c0ae80
hannako <jessjones1@gmail.com>
2016-07-20 18:11:40 +0100

    challenge 26: refactoring controller to pass all logic through the Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,19 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new }
-  let(:mannie) { double('Mannie') }
-  let(:jess) { double('Player') }
+  subject(:game) { described_class.new(player_1, player_2)  }
+  let(:player_1) { double('Player') }
+  let(:player_2) { double('Player') }
 
   describe '#attack' do
     it 'damages your opponent' do
-      expect(jess).to receive(:receive_damage)
-      game.attack(jess)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
     end
   end
+
+  
+
+  it { is_expected.to respond_to(:player_1) }
 
 end
