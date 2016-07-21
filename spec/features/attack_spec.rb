f9dc23252e2c818fdae001732c52d34d37b50f71
Adam <adamjbrowner@gmail.com>
2016-07-21 11:47:34 +0100

    Refactored controller to remove a global variable

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,12 +4,4 @@ feature 'Attacking' do
     click_button 'Attack'
     expect(page).to have_content 'Joe attacked Oggie'
   end
-
-  scenario 'reduce points after attack' do
-    sign_in_and_play
-    click_button 'Attack'
-    # click_button 'OK'
-    expect(page).not_to have_content 'Oggie: 60HP'
-    expect(page).to have_content 'Oggie: 50HP'
-  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,9 +2,18 @@ require 'game'
 require 'player'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player_1, player_2) }
   let(:player_1) { double :player }
   let(:player_2) { double :player }
+
+  describe '#initialize' do
+    it "returns player_1" do
+      expect(game.player_1).to eq(player_1)
+    end
+    it "returns player_2" do
+      expect(game.player_2).to eq(player_2)
+    end
+  end
 
   describe '#attack' do
     it 'damages the player' do
@@ -12,4 +21,4 @@ describe Game do
       game.attack(player_2)
     end
   end
-end 
+end
